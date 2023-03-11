af0d1cde3250f216f4af48f718fe9b67354d6ff6
George <badjokeresult@gmail.com>
2023-03-11 17:19:11 +0300

    settings

--- Program.cs
+++ Program.cs
@@ -1,15 +1,20 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.EntityFrameworkCore;
+using TicTacToe.DatabaseContext;
 
-// Add services to the container.
+var builder = WebApplication.CreateBuilder(args);
+builder.Logging.ClearProviders().AddConsole();
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+var connection = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<AppDbContext>(opt =>
+    opt.UseNpgsql(connection));
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
