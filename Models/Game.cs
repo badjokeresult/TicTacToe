ab934a24cf55f5a5cc6b9a678fc71e822692c03c
George <badjokeresult@gmail.com>
2023-03-12 17:30:59 +0300

    added-ctors

--- Models/Game.cs
+++ Models/Game.cs
@@ -1,8 +1,19 @@
+using Newtonsoft.Json.Linq;
+
 namespace TicTacToe.Models;
 
 public class Game
 {
     public int Id { get; set; }
-    public Player FirstPlayer { get; set; }
-    public Player SecondPlayer { get; set; }
+    public Player FirstPlayer { get; set; } = null!;
+    public Player SecondPlayer { get; set; } = null!;
+
+    public Game(string json)
+    {
+        var jObject = JObject.Parse(json);
+        var jGame = jObject["game"];
+        var players = jGame["players"].ToArray();
+        FirstPlayer = new Player(players[0].ToString());
+        SecondPlayer = new Player(players[1].ToString());
+    }
 }
