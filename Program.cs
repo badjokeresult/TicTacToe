71b92490197f038578ea03c02f3497b1ba0828bd
George <badjokeresult@gmail.com>
2023-03-13 00:23:16 +0300

    remove-swagger

--- Program.cs
+++ Program.cs
@@ -15,15 +15,16 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseCors(b => b
+    .AllowAnyOrigin()
+    .AllowAnyMethod()
+    .AllowAnyHeader());
 
-app.UseHttpsRedirection();
+app.UseAuthentication();;
 
 app.UseAuthorization();
+
+app.UseRouting();
 
 app.MapControllers();
 
